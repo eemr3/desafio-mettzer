499fe018b3075d78ef9633d753e77ba1b5eee643
eemr3 <eemr3@yahoo.com.br>
2022-09-02 14:42:43 -0300

    feat(service de user): adiciona verificação de usuário existente e incriptação da senha no banco de dados

--- back-end/src/controllers/User.controller.js
+++ back-end/src/controllers/User.controller.js
@@ -1,9 +1,14 @@
 const Service = require('../services/User.service');
 
 const createUser = async (req, res) => {
-  const user = await Service.createUser(req.body);
+  try {
+    const user = await Service.createUser(req.body);
 
-  return res.status(201).json(user);
+    return res.status(201).json(user);
+  } catch (error) {
+    console.log(error);
+    return res.status(error.status).json({ message: error.message });
+  }
 };
 
 module.exports = {
--- back-end/src/services/User.service.js
+++ back-end/src/services/User.service.js
@@ -1,7 +1,17 @@
+const bcrypt = require('bcryptjs');
 const { User } = require('../database/models');
+const errorBase = require('../utils/errorBase');
 
 const createUser = async (data) => {
-  const user = User.create(data);
+  const { name, email, password } = data;
+  const userExists = await User.findOne({ where: { email } });
+  if (userExists) {
+    throw errorBase(409, 'Usuário já cadastrado');
+  }
+
+  const newPwd = await bcrypt.hash(password, 10);
+
+  const user = await User.create({ name, email, password: newPwd });
 
   return user;
 };
