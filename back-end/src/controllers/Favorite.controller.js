6895640e4e208c871fbd425562caea7bb9b461cf
eemr3 <eemr3@yahoo.com.br>
2022-09-02 17:49:57 -0300

    feat(Favorite.controller.js): cria controller de favorite

---
+++ back-end/src/controllers/Favorite.controller.js
@@ -0,0 +1,37 @@
+const Service = require('../services/Favorite.service');
+
+const createFavorite = async (req, res) => {
+  const { authors, types, title, description, urls } = req.body;
+  const { id } = req.data;
+  const favorite = await Service.createFavorite({
+    authors,
+    types,
+    title,
+    description,
+    urls,
+    id,
+  });
+
+  return res.status(201).json(favorite);
+};
+
+const getAllFavorites = async (_req, res) => {
+  const favorites = await Service.getAllFavorites();
+
+  return res.status(200).json(favorites);
+};
+
+const destroyFovorite = async (req, res) => {
+  try {
+    await Service.destroyFovorite(req.params.id);
+    return res.status(200).json({ message: 'Desfavoritado com sucesso' });
+  } catch (error) {
+    return res.status(error.status).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  createFavorite,
+  getAllFavorites,
+  destroyFovorite,
+};
