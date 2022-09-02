f05ef8de4ad57a86205ef0a9844b0131f16e4df3
eemr3 <eemr3@yahoo.com.br>
2022-09-02 14:11:49 -0300

    feat(app.js): adiciona a chamada para a rota de "/usres"

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const cors = require('cors');
+const Router = require('../routes');
+
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (_req, res) => res.send('Hello World!'));
-
+app.use('/users', Router.UserRouter);
 module.exports = app;
