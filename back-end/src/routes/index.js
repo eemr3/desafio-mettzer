4dfbab26c2266d1448bcce61560429b0e982165e
eemr3 <eemr3@yahoo.com.br>
2022-09-02 17:50:44 -0300

    feat(routes/index.js): adiciona rota "/favorites"

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,7 +1,9 @@
 const UserRouter = require('./User/User.routes');
 const SignInRouter = require('./SignIn/SingIn.routes');
+const FavoriteRouter = require('./Favorite/Favorite.routes');
 
 module.exports = {
   UserRouter,
   SignInRouter,
+  FavoriteRouter,
 };
