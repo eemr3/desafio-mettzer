b7a3014cf10d799fa941ff1fbe6ecdb72b2868a4
eemr3 <eemr3@yahoo.com.br>
2022-09-02 14:06:23 -0300

    feat(models/user.js, migrations/user.js): cria model e migration de users

---
+++ back-end/src/database/models/user.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+    },
+    {
+      tableName: 'users',
+    },
+  );
+  return User;
+};
