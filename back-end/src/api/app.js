1760ba97044bac955bdd26b8d1ef17230520837a
eemr3 <eemr3@yahoo.com.br>
2022-09-02 14:55:19 -0300

    feat(app.js): adiciona a chamanda para a rota de "/login"

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -7,5 +7,8 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (_req, res) => res.send('Hello World!'));
+
 app.use('/users', Router.UserRouter);
+app.use('/login', Router.SignInRouter);
+
 module.exports = app;
