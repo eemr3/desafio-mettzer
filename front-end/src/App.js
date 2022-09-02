ee4c5be744336f84251d975640dbe0aa7ca70623
eemr3 <eemr3@yahoo.com.br>
2022-09-02 20:00:33 -0300

    feat(App.js): adiciona o provider ao app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,12 @@
+import AuthProvider from './context/AuthProvider';
 import AppRoutes from './routes/routes';
 
 function App() {
   return (
     <div className="h-screen">
-      <AppRoutes />
+      <AuthProvider>
+        <AppRoutes />
+      </AuthProvider>
     </div>
   );
 }
