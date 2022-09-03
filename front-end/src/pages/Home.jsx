6f4ff24327c71e137ab91e862f30e40e8fae1ea7
eemr3 <eemr3@yahoo.com.br>
2022-09-02 23:09:16 -0300

    feat(Home.jsx): adiciona o componente de paginação

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,15 +1,17 @@
 import React, { useContext } from 'react';
+import Pagination from '../components/Pagination';
 import SearchBar from '../components/SearchBar';
 import Table from '../components/Table';
 import { AppContext } from '../context/AppContext';
 
+const LIMIT = 10;
 function Home() {
   const { offset, setOffset } = useContext(AppContext);
   return (
     <>
       <SearchBar />
       <Table />
-      {/* <Pagination offset={offset} limit={LIMIT} total={100} setOffset={setOffset} /> */}
+      <Pagination offset={offset} limit={LIMIT} total={100} setOffset={setOffset} />
     </>
   );
 }
