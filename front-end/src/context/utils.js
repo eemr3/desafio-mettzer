84a3d4ada3baf6253c2f2a2e63bdb559243d009f
eemr3 <eemr3@yahoo.com.br>
2022-09-02 22:11:49 -0300

    feat(utils.js): adiciona função que realiza requisição a api "CORE API"

--- front-end/src/context/utils.js
+++ front-end/src/context/utils.js
@@ -1,7 +1,17 @@
 import { api } from '../api/api';
+import { ExternalApi } from '../api/externalApi';
 
 export const requestSignIn = async (values) => {
   const response = await api.post('/login', values);
 
   return response.data;
+};
+
+export const getListArticle = async (query, page, key) => {
+  const response = await ExternalApi.get(
+    `/search/${query}?page=${page}&pageSize=10&apiKey=${key}`,
+  );
+  console.log(response);
+
+  return response.data.data;
 };
