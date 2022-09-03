6366c1aeb0648b24355758aa51f7e91f0dc62551
eemr3 <eemr3@yahoo.com.br>
2022-09-03 16:18:49 -0300

    feat(utils.js): adiciona função que realiza request para a api no endpoint "/favorites"

--- front-end/src/context/utils.js
+++ front-end/src/context/utils.js
@@ -1,3 +1,4 @@
+import Cookies from 'js-cookie';
 import { api } from '../api/api';
 import { ExternalApi } from '../api/externalApi';
 
@@ -7,10 +8,20 @@ export const requestSignIn = async (values) => {
   return response.data;
 };
 
-export const getListArticle = async (query, page, key) => {
+export const getListArticle = async (query, page) => {
+  const KEY = process.env.REACT_APP_APIKEY_CORE;
   const response = await ExternalApi.get(
-    `/search/${query}?page=${page === 0 ? 1 : page}&pageSize=10&apiKey=${key}`,
+    `/search/${query}?page=${page === 0 ? 1 : page}&pageSize=10&apiKey=${KEY}`,
   );
 
   return response.data.data;
+};
+
+export const requestSaveFavorite = async (data) => {
+  const token = Cookies.get('token');
+  await api.post('/favorites', data, {
+    headers: {
+      authorization: token,
+    },
+  });
 };
