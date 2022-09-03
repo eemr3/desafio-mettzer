fa81a75e0f057e4ba9dfce39717add6bfb79869e
eemr3 <eemr3@yahoo.com.br>
2022-09-02 22:09:39 -0300

    feat(HomeProvider.jsx): cria provider para home

---
+++ front-end/src/context/HomeProvider.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { AppContext } from './AppContext';
+import { getListArticle } from './utils';
+
+const KEY = process.env.REACT_APP_APIKEY_CORE;
+
+function HomeProvider({ children }) {
+  const [articles, setArticles] = useState([]);
+  const [query, setQuery] = useState('Água');
+  const [inputChange, setInputChange] = useState('');
+  const [offset, setOffset] = useState(1);
+
+  useEffect(() => {
+    const allListArticle = async () => {
+      const result = await getListArticle(query, offset, KEY);
+
+      setArticles(result);
+    };
+    allListArticle();
+  }, [offset, query]);
+
+  return (
+    <AppContext.Provider
+      value={{
+        articles,
+        setQuery,
+        setInputChange,
+        inputChange,
+        offset,
+        setOffset,
+      }}>
+      {children}
+    </AppContext.Provider>
+  );
+}
+
+export default HomeProvider;
