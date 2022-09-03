0052f0ffaa9e0e0f1834c0f558c6efe6dfd0342c
eemr3 <eemr3@yahoo.com.br>
2022-09-03 16:22:35 -0300

    feat(ButtonFavorites.jsx): adiciona função para salvar os dados ao favoritos

--- front-end/src/components/ButtonFavorite.jsx
+++ front-end/src/components/ButtonFavorite.jsx
@@ -1,15 +1,37 @@
 import React, { useState } from 'react';
+import { useContext } from 'react';
 import { IoHeartOutline, IoHeartSharp } from 'react-icons/io5';
+import { AppContext } from '../context/AppContext';
+import { requestSaveFavorite } from '../context/utils';
 
-export default function ButtonFavorite() {
+export default function ButtonFavorite(favId) {
+  const { articles } = useContext(AppContext);
   const [isFavorite, setIsFavorite] = useState(false);
+
+  const handleSaveFavorite = async () => {
+    const result = articles.find(
+      (item) => Number(item._source.id) === Number(favId.favData),
+    );
+    const data = {
+      idArticle: result._source.id,
+      authors: result._source.authors,
+      type: result._type,
+      title: result._source.title,
+      description: result._source.description,
+      urls: result._source.urls,
+    };
+
+    await requestSaveFavorite(data);
+    setIsFavorite(!isFavorite);
+  };
+
   return isFavorite ? (
     <p className="text-center">
       <IoHeartSharp
         className="cursor-pointer"
         color="rgb(220 38 38)"
         size="2rem"
-        onClick={() => setIsFavorite(!isFavorite)}
+        onClick={() => {}}
       />
     </p>
   ) : (
@@ -17,7 +39,7 @@ export default function ButtonFavorite() {
       <IoHeartOutline
         size="2rem"
         className="cursor-pointer"
-        onClick={() => setIsFavorite(!isFavorite)}
+        onClick={() => handleSaveFavorite()}
       />
     </p>
   );
