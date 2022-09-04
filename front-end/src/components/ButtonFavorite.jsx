be773f9adbb9fca39433fa2bcff083743af469fc
eemr3 <eemr3@yahoo.com.br>
2022-09-04 16:49:09 -0300

    feat(ButtonFavorites.jsx): adiciona função de desfavoritar

--- front-end/src/components/ButtonFavorite.jsx
+++ front-end/src/components/ButtonFavorite.jsx
@@ -1,28 +1,42 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useContext } from 'react';
 import { IoHeartOutline, IoHeartSharp } from 'react-icons/io5';
 import { AppContext } from '../context/AppContext';
-import { requestSaveFavorite } from '../context/utils';
+import { requestDeleteFavorite, requestSaveFavorite } from '../context/utils';
 
-export default function ButtonFavorite(favId) {
-  const { articles } = useContext(AppContext);
-  const [isFavorite, setIsFavorite] = useState(false);
+export default function ButtonFavorite({ favData, checked }) {
+  const { articles, favorites, favorited, setFavorited } = useContext(AppContext);
+  const [isFavorite, setIsFavorite] = useState(checked);
 
   const handleSaveFavorite = async () => {
-    const result = articles.find(
-      (item) => Number(item._source.id) === Number(favId.favData),
-    );
+    const result = articles.find((item) => Number(item.id) === Number(favData));
     const data = {
-      idArticle: result._source.id,
-      authors: result._source.authors,
-      type: result._type,
-      title: result._source.title,
-      description: result._source.description,
-      urls: result._source.urls,
+      idArticle: result.id,
+      authors: result.authors,
+      _type: result._type,
+      title: result.title,
+      description: result.description,
+      urls: result.urls,
     };
 
-    await requestSaveFavorite(data);
-    setIsFavorite(!isFavorite);
+    try {
+      await requestSaveFavorite(data);
+      setIsFavorite(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleUnfavorite = async () => {
+    const result = favorites.find((item) => item.id === Number(favData));
+    console.info(result);
+    try {
+      await requestDeleteFavorite(result.idFav);
+      setIsFavorite(false);
+      setFavorited(!favorited);
+    } catch (error) {
+      console.info(error);
+    }
   };
 
   return isFavorite ? (
@@ -31,7 +45,7 @@ export default function ButtonFavorite(favId) {
         className="cursor-pointer"
         color="rgb(220 38 38)"
         size="2rem"
-        onClick={() => {}}
+        onClick={handleUnfavorite}
       />
     </p>
   ) : (
@@ -39,7 +53,7 @@ export default function ButtonFavorite(favId) {
       <IoHeartOutline
         size="2rem"
         className="cursor-pointer"
-        onClick={() => handleSaveFavorite()}
+        onClick={handleSaveFavorite}
       />
     </p>
   );
