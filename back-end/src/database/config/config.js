57fff54ed15c58fec85953a9eba3a31b7564f49c
eemr3 <eemr3@yahoo.com.br>
2022-09-04 18:32:33 -0300

    feat(config/config.js): adiciona variavel de ambiente

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
-    username: 'root',
-    password: '12345678',
-    database: 'mettzer_db',
-    host: '127.0.0.1',
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.HOSTNAME,
     dialect: 'mysql',
   },
   test: {
